7b2d503c3f018c3894b1b870cb9328c01ad29fb2
Flatiron <flatiron@Flatirons-MacBook-Air-2.local>
2019-01-17 16:02:02 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,34 +2,52 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
-  end
-  
+        sql = <<-SQL
+          SELECT *
+          FROM students
+          WHERE name = ?
+          LIMIT 1
+        SQL
+
+        DB[:conn].execute(sql, name).map do |row|
+          self.new_from_db(row)
+        end.first
+      end
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade TEXT
+      grade INTEGER
     )
     SQL
 
@@ -40,4 +58,42 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    DB[:conn].execute("Select * from students where students.grade = 9");
+  end
+
+
+  def self.students_below_12th_grade
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade < 12
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        new_from_db(row)
+      end
+    end
+
+  # to reference function arguments inside of an SQL command, you should set the argument to ?, argument_name
+  def self.first_X_students_in_grade_10(grade)
+    DB[:conn].execute("Select * from students where students.grade = 10 limit ?", grade);
+  end
+
+  def self.all_students_in_grade_X(grade)
+    DB[:conn].execute("Select * from students where students.grade = ?", grade);
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      new_from_db(row)
+    end.first
+  end
+  #class end
 end
